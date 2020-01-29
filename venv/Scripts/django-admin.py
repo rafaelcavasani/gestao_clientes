20c2ef7eed69f1880b53fbce61b5553977831780
Rafael Cavasani <rafael.cavasani@gmail.com>
2020-01-28 23:01:05 -0300

    Projeto quase finalizado

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\rafael cavassani\documents\programacao\python\gestao_clientes\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
